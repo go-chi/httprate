9d896079ebb137df8a1e1a6d0779cb3fdc0e023d
Peter Kieltyka <peter.kieltyka@gmail.com>
2022-03-01 05:20:26 -0500

    simplify canonicalizeIP

--- httprate.go
+++ httprate.go
@@ -84,7 +84,7 @@ func composedKeyFunc(keyFuncs ...KeyFunc) KeyFunc {
 	}
 }
 
-// CanonicalizeIP returns a form of ip suitable for comparison to other IPs.
+// canonicalizeIP returns a form of ip suitable for comparison to other IPs.
 // For IPv4 addresses, this is simply the whole string.
 // For IPv6 addresses, this is the /64 prefix.
 func canonicalizeIP(ip string) string {
@@ -107,26 +107,10 @@ func canonicalizeIP(ip string) string {
 		return ip
 	}
 
-	// By default, the string representation of a net.IPNet (masked IP address) is just
-	// "full_address/mask_bits". But using that will result in different addresses with
-	// the same /64 prefix comparing differently. So we need to zero out the last 64 bits
-	// so that all IPs in the same prefix will be the same.
-	//
-	// Note: When 1.18 is the minimum Go version, this can be written more cleanly like:
-	// netip.PrefixFrom(netip.MustParseAddr(ipv6), 64).Masked().Addr().String()
-	// (With appropriate error checking.)
-
 	ipv6 := net.ParseIP(ip)
 	if ipv6 == nil {
 		return ip
 	}
 
-	const bytesToZero = (128 - 64) / 8
-	for i := len(ipv6) - bytesToZero; i < len(ipv6); i++ {
-		ipv6[i] = 0
-	}
-
-	// Note that this doesn't have the "/64" suffix customary with a CIDR representation,
-	// but those three bytes add nothing for us.
-	return ipv6.String()
+	return ipv6.Mask(net.CIDRMask(64, 128)).String()
 }
