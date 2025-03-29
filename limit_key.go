c0b6272bcac673c105a4d29c98dd9be27a0d644d
Peter Kieltyka <pkieltyka@users.noreply.github.com>
2025-03-29 16:29:09 -0400

    upgrade to xxh3 hashing pkg (#54)

--- limit_key.go
+++ limit_key.go
@@ -4,11 +4,11 @@ import (
 	"fmt"
 	"time"
 
-	"github.com/cespare/xxhash/v2"
+	"github.com/zeebo/xxh3"
 )
 
 func LimitCounterKey(key string, window time.Time) uint64 {
-	h := xxhash.New()
+	h := xxh3.New()
 	h.WriteString(key)
 	h.WriteString(fmt.Sprintf("%d", window.Unix()))
 	return h.Sum64()
